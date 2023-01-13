7db6d08ed751932e6ea0e949275e1be93e113178
bianca florescu <biancaflorescu97@yahoo.com>
2023-01-13 17:43:38 +0200

    added new functionality and resolved bugs

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -1,47 +1,81 @@
 import { useRef, useState } from "react";
+import usePrevious from "../../customHooks/usePrevious";
 import "./calculator.css";
 
 const Calculator = () => {
   const [operation, setOperation] = useState("");
-  const sign = useRef("");
+  const [sign, setSign] = useState("");
+  const counter = useRef(0);
+  const pointCounter = useRef(0);
 
   const writeText = (e) => {
-    const text = e.target.innerText;
+    let text = e.target.innerText;
+
+    if (text === "+" || text === "-" || text === "*" || text === "/") {
+      setSign(text);
+      counter.current += 1;
+      pointCounter.current = 0;
+    }
+
+    if (text === ".") {
+      pointCounter.current += 1;
+    }
+
+    if (text === "." && pointCounter.current > 1) {
+      text = "";
+    }
+
     setOperation((prev) => prev + text);
 
-    if (text === "+") {
-      sign.current = "+";
-    } else if (text === "-") {
-      sign.current = "-";
-    } else if (text === "*") {
-      sign.current = "*";
-    } else if (text === "/") {
-      sign.current = "/";
+    if (counter.current === 2) {
+      calcOperation();
+      setOperation((prev) => prev + text);
+      counter.current = 1;
     }
   };
 
+  const prevSign = usePrevious(sign);
+
   const deleteText = () => {
-    setOperation((prev) => prev.slice(0, -1));
+    if (counter.current !== 1) {
+      setOperation((prev) => prev.slice(0, -1));
+    }
   };
 
   const resetCalc = () => {
     setOperation("");
+    setSign("");
+    counter.current = 0;
+    pointCounter.current = 0;
   };
 
   const calcOperation = () => {
+    let signIndex = 0;
+
+    counter.current = 1;
+
+    for (let i = 0; i < operation.length; i++) {
+      if (isNaN(Number(operation[i])) && operation[i] !== ".") {
+        signIndex = operation.indexOf(operation[i]);
+      }
+    }
+
+    console.log(counter);
     console.log(operation);
-    console.log(sign.current);
-    const signIndex = operation.indexOf(sign.current);
+    console.log(prevSign);
+
     const firstNumber = operation.slice(0, signIndex);
     const secondNumber = operation.slice(signIndex + 1);
 
-    if (sign.current === "+") {
+    console.log(signIndex, firstNumber, secondNumber);
+
+    if (prevSign === "+") {
       setOperation(+firstNumber + +secondNumber);
-    } else if (sign.current === "-") {
+    } else if (prevSign === "-") {
       setOperation(+firstNumber - +secondNumber);
-    } else if (sign.current === "*") {
+    } else if (prevSign === "*") {
       setOperation(+firstNumber * +secondNumber);
-    } else if (sign.current === "/") {
+    } else if (prevSign === "/") {
       setOperation(+firstNumber / +secondNumber);
     }
   };
